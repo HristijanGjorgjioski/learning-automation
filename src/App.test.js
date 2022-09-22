9d8a719c6fdf2d00701b920499de5071d6d44edd
Gjorgjioski Hristijan <76740146+HristijanGjorgjioski@users.noreply.github.com>
2022-09-22 19:56:00 +0200

    Lets test (#5)
    * Update App.test.js
    
    * Update App.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders Hristijan Tes text', () => {
   render(<App />);
-  const textElement = screen.getByText('Hristijan Test');
+  const textElement = screen.getByText('Hristijan');
   expect(textElement).toBeInTheDocument();
 });
